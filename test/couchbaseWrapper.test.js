e0c501e7a255678bbfdd5607d86b16fde7f0551b
Dimosthenis Botsaris <botsaris.d@gmail.com>
2017-10-03 10:52:31 +0300

    add more tests and cluster them in a better way

--- test/couchbaseWrapper.test.js
+++ test/couchbaseWrapper.test.js
@@ -40,53 +40,65 @@ describe('test connection:', function() {
 
 
 describe('test functions to handle the result of the asynchronous operations:', function() {
-  it('should be able to get doc from local DB bucket', function (done) {
-    couchbaseWrapper.getDoc('users', 'user:dimostest1')
-      .then((userDoc) => {
-        expect(userDoc.value).to.not.be.empty;
-        expect(userDoc.value).to.be.an('object');
-        expect(userDoc.value.name).to.eql('dimostest1');
-        return done();
-      });
+
+  describe('test getDoc functions ', function() {
+    it('should be able to get doc from local DB bucket', function (done) {
+      couchbaseWrapper.getDoc('users', 'user:dimostest1')
+        .then((userDoc) => {
+          expect(userDoc.value).to.not.be.empty;
+          expect(userDoc.value).to.be.an('object');
+          expect(userDoc.value.name).to.eql('dimostest1');
+          return done();
+        });
+    });
+
+    it('should not be able to get doc from local DB bucket as the doc does not exist', function (done) {
+      couchbaseWrapper.getDoc('users', 'user:dimostestnew')
+        .catch((error) => {
+          expect(error.code).to.be.equal(13);
+          return done();
+        });
+    });
+
+    it('should return error as we try get doc from bucket without connection', function (done) {
+      couchbaseWrapper.getDoc('usersTestBucket', 'user:dimostestnew')
+        .catch((error) => {
+          expect(error).to.be.equal('No bucket connection for usersTestBucket');
+          return done();
+        });
+    });
   });
 
-  it('should not be able to get doc from local DB bucket as the doc does not exist', function (done) {
-    couchbaseWrapper.getDoc('users', 'user:dimostestnew')
-      .catch((error) => {
-        expect(error.code).to.be.equal(13);
-        return done();
-      });
+  describe('test insertDoc functions ', function() {
+    it('should insert new doc', function (done) {
+      couchbaseWrapper.insertDoc('users', 'user:dimostestOnlyForTest', newUserValue)
+        .then((userDoc) => {
+          expect(userDoc.cas).to.not.be.empty;
+          return couchbaseWrapper.getDoc('users', 'user:dimostestOnlyForTest');
+        })
+        .then((userDoc) => {
+          expect(userDoc.value).to.not.be.empty;
+          expect(userDoc.value.name).to.eql(newUserValue.name);
+          return done();
+        });
+    });
+
+    it('should return error in insert new doc because this doc exist', function (done) {
+      couchbaseWrapper.insertDoc('users', 'user:dimostestOnlyForTest', newUserValue)
+        .catch((error) => {
+          expect(error.code).to.be.equal(12);
+          return done();
+        });
+    });
   });
 
-  it('should return error as we try get doc from bucket without connection', function (done) {
-    couchbaseWrapper.getDoc('usersTestBucket', 'user:dimostestnew')
-      .catch((error) => {
-        expect(error).to.be.equal('No bucket connection for usersTestBucket');
-        return done();
-      });
-  });
-
-  it('should insert new doc', function (done) {
-    couchbaseWrapper.insertDoc('users', 'user:dimostestOnlyForTest', newUserValue)
-      .then((userDoc) => {
-        expect(userDoc.cas).to.not.be.empty;
-        return done();
-      });
-  });
-
-  it('should return error in insert new doc because this doc exist', function (done) {
-    couchbaseWrapper.insertDoc('users', 'user:dimostestOnlyForTest', newUserValue)
-      .catch((error) => {
-        expect(error.code).to.be.equal(12);
-        return done();
-      });
-  });
-
-  it('should remove doc from DB', function (done) {
-    couchbaseWrapper.removeDoc('users', 'user:dimostestOnlyForTest')
-      .then((userDoc) => {
-        expect(userDoc.cas).to.not.be.empty;
-        return done();
-      });
+  describe('test removeDoc functions ', function() {
+    it('should remove doc from DB', function (done) {
+      couchbaseWrapper.removeDoc('users', 'user:dimostestOnlyForTest')
+        .then((userDoc) => {
+          expect(userDoc.cas).to.not.be.empty;
+          return done();
+        });
+    });
   });
 });
